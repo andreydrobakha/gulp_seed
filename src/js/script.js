44b9487ea8b151db4a656f2ea1c9d442c6f2e2b1
andrey_drobakha <gerrard881202@gmail.com>
2018-11-18 11:52:28 +0200

    testing_gulp_seed

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+// let userName = prompt('What is your name?');
+
+
+// console.log(`Hello, ${userName}`);
+
+// console.log('Hello, ' + userName);
+
+// const num1 = 6;
+// const num2 = 12;
+
+// if(num1 > num2) {
+// 	console.log('part 1');
+// } else if (num1 === num2) {
+// 	console.log('part 2');
+// } else {
+// 	console.log('part 3')
+// }
+
+// switch 
+// loop
+
+// const age = prompt('How old are you?');
+
+// const a = age >= 18 ? true : false
+
+// let b;
+
+// const f = function(a, b) => {
+	
+// }
+
+// это тернанное выражение
